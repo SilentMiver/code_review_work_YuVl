78e0efc40bbc5d40f2d910a01dd76f1e36373d8f
YuriDiachenko <d.yurok22@mail.ru>
2023-11-25 16:20:44 +0300

    Make realisation of math operations

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return (a + b);
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int diff(int a, int b){
+        return (a - b);
     }
+
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b != 0) {
+            return (a / b);
+        } else {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
     }
+
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return (a * b);
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, int c){
+        return sum(times(b, c), a);
     }
 }
+
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,48 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(2, 3);
+
+        assertEquals(5, result);
     }
 
     @Test
-    void dif() {
-        //TODO inser your realisation in test-method dif
+    void diff() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.diff(5, 2);
+
+        assertEquals(3, result);
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        
+        int result1 = calculator.div(6, 2);
+
+        assertEquals(3, result1);
+        assertThrows(ArithmeticException.class, () -> calculator.div(5, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+
+        int result = calculator.times(4, 3);
+
+        assertEquals(12, result);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+
+        // x = (b * c) + a
+        int result = calculator.solver(2, 3, 4);
+
+        assertEquals(14, result);
     }
 }
