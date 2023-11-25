51c31aec0b9dce92a5f93154d75593f77d11777c
russi <russianthief3@gmail.com>
2023-11-25 16:20:10 +0300

    add realisation of math methods in Calculator.java and JUnit teata in CalculatorTest.java change README.md

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int diff(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, int c) {
+        // a-x = b * c
+        return add(a, times(b, c));
     }
 }
